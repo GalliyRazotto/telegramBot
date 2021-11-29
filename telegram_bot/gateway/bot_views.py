c28b48a8dc710699cfd3e97307b5f39d615b0b22
Kirill <yeahkirill@gmail.com>
2021-11-29 17:03:44 +0300

    request added, config_bot excluded from gitignore, .env added

--- telegram_bot/gateway/bot_views.py
+++ telegram_bot/gateway/bot_views.py
@@ -1,6 +1,6 @@
 import logging
 
-from aiogram import Bot, Dispatcher, executor, types
+from aiogram import Bot, Dispatcher, types
 from aiogram.dispatcher.filters import CommandStart
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
@@ -8,7 +8,7 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 from telegram_bot.keyboards.client_keyboard import kb_client
 
-from bot_config import TOKEN
+from config_bot import TOKEN
 import telegram_bot.usecase.add_words_users as usecase
 
 logging.basicConfig(level=logging.INFO)
@@ -29,16 +29,6 @@ async def cm_add_word(message: types.Message):
     await message.reply('Input word to translate')
 
 
-@dp.message_handler(commands='request', state=None)
-async def request_word(message: types.Message):
-    user_name = message.from_user.first_name
-    msg = 'No words added to dictionary'
-    word = usecase.request_random_word(user_name)
-    if word:
-        msg = str(word.word)
-    await message.reply(msg)
-
-
 @dp.message_handler(content_types=['text'], state=FSMAdmin.add_word)
 async def pull_word(message: types.Message, state: FSMContext):
     word = message.text.lower()
@@ -48,6 +38,30 @@ async def pull_word(message: types.Message, state: FSMContext):
         async with state.proxy() as data:
             data['word'] = word
         msg = 'Word added'
+    await message.reply(msg)
+    await state.finish()
+
+
+@dp.message_handler(commands='request', state=None)
+async def request_word(message: types.Message, state: FSMContext):
+    user_name = message.from_user.first_name
+    msg = 'No words added to dictionary'
+    word = usecase.request_random_word(user_name)
+    await FSMAdmin.request_word.set()
+    if word:
+        async with state.proxy() as data:
+            data['word'] = word.translation
+            msg = f'Напишите перевод слова {word.word}'
+    await message.reply(msg)
+
+
+@dp.message_handler(content_types=['text'], state=FSMAdmin.request_word)
+async def pull_word(message: types.Message, state: FSMContext):
+    translation = message.text.lower()
+    msg = 'You should learn it again. Would you like to /request once more?'
+    async with state.proxy() as data:
+        if translation == data['word']:
+            msg = 'Great! /request to try once more!'
     await message.reply(msg)
     await state.finish()
 
